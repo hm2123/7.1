ad7d3a4bf77b7712700bd978436a046f27cae8e4
hamid <hm2123@email.vccs.edu>
2017-12-07 13:33:44 -0500

    Initial commit

---
+++ src/Sort.java
@@ -0,0 +1,19 @@
+/**
+ * Creator: Hamid Majeed
+ */
+public class Sort {
+    public static void swap(int oneNum, int otherNum, int [] numArr) {
+        int saveIdx = numArr[oneNum];
+        numArr[oneNum] = numArr[otherNum];
+        numArr[otherNum] = saveIdx;
+    }
+    public static void sort(int [] numArr) {
+        for(int i = 0; i < numArr.length; i++) {
+            for(int x = i; x > 0; x--) {
+                if(numArr[x] < numArr[x-1]) {
+                    Sort.swap(x, x-1, numArr);
+                }
+            }
+        }
+    }
+}
